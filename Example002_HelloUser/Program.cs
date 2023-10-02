15486e3cbe9ca0fd5fc0c9ce1454374dc28d50d9
Vit <mayllby@gmail.com>
2023-10-02 10:15:40 +0300

    Example 003 Sum

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывод текста столбиком ("WriteLine"):
+//Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+// Вывод текста одной строкой ("Write")
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
---
+++ Example004_Div/Program.cs
@@ -0,0 +1,12 @@
+﻿// Деление чисел
+// "int" (integer) - контейнер хранения целых чисел
+// int numberA = 10;
+// int numberB = 5;
+// int result = numberA / numberB;
+// Console.WriteLine(result);
+
+// "double" - контейнер хранения дробей
+double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+Console.WriteLine(result);
