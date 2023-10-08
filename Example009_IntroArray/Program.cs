8df11779c85aec6ca45abb990d795e9fb6564668
Vit <mayllby@gmail.com>
2023-10-08 13:00:01 +0300

    Изменил способ решения задачи 1

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Лекция 2. Знакомство с функциями и массивами
 
+
 // 0:21:30
 // * Массивы
 
 // Задача 1
-// * Решение с помощью функций
 
 // Пишем код поиска максимума из трёх аргументов
 // Наличие кода, позволяет вносить необходимые правки в запись в одном месте
@@ -12,26 +12,20 @@
 
 int Max(int arg1,int arg2, int arg3)
 {
-    int resalt = arg1;
-    if(arg2 > resalt) resalt = arg2;
-    if(arg3 > resalt) resalt = arg3;
-    return resalt;
-}
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+} //            0   1   2    3   4    5     6   7     8 // индексы массива
+int[] array = {11, 21, 31, 411, 15, 612, 1117, 18, 1911};
 
-int a1 = 13;
-int b1 = 21;
-int c1 = 39;
-int a2 = 1211111;
-int b2 = 23;
-int c2 = 32123;
-int a3 = 13;
-int b3 = 2113;
-int c3 = 313;
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
-
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
-Console.WriteLine(max);
+// можно дать любое имя переменной, присваивающей значение "Max(...)",
+// например, изменить "max" на "result"
+// int max = Max( 
+int result = Max(
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8])
+);
+// Console.WriteLine(max);
+Console.WriteLine(result);
