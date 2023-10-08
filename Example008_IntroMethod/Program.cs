2f01e525ff676167070c18f97a36eba2e54efb2c
Vit <mayllby@gmail.com>
2023-10-08 11:15:32 +0300

    исправил код в задаче 1

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -5,6 +5,10 @@
 // Задача 1
 // * Решение с помощью функций
 
+// Пишем код поиска максимума из трёх аргументов
+// Наличие кода, позволяет вносить необходимые правки в запись в одном месте
+// Решение стало более универсальным 
+
 int Max(int arg1,int arg2, int arg3)
 {
     int resalt = arg1;
@@ -16,17 +20,19 @@ int Max(int arg1,int arg2, int arg3)
 int a1 = 13;
 int b1 = 21;
 int c1 = 39;
-int a2 = 12;
+int a2 = 1211111;
 int b2 = 23;
 int c2 = 32123;
 int a3 = 13;
 int b3 = 2113;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(max);
 
 // * Решение стихийным методом
