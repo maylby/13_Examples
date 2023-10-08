91b25860e8d085e7be2f45a283db5f99bd5915e9
Vit <mayllby@gmail.com>
2023-10-08 15:21:28 +0300

    Добавил методы 'FillArray' и 'PrintArray'

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -2,9 +2,9 @@
 
 // 0:33:00
 
-int[] array = new int[10]; // Смысл конструкции "new int[10]": создать новый массив из 10 элементов
+// * Код создания массива
 
-void FillArray(int[] collection)
+void FillArray(int[] collection) // "void" - метод, который ничего не возвращает
 {
     int length = collection.Length;
     int index = 0;
@@ -12,6 +12,25 @@ void FillArray(int[] collection)
     {
         collection[index] = new Random().Next(1, 10);
         // В агрумент "collection" на позицию "index" положить новое случайное целое число от 1 до 10
-        index++;
+        index++; // В методе "void" не применим оператор "return"
     }
 }
+
+// * Код вывода массива в печать (на экран)
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int[] array = new int[10]; // Смысл конструкции "new int[10]" - создать новый массив из 10 элементов
+
+FillArray(array); // Методу "FillArray" передаём массив "array"
+PrintArray(array); // "PrintArray" распечатает переданный массив
+
