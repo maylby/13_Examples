57fee689c0c9c0ecf3194d7f6927743ecbf7a75c
Vit <mayllby@gmail.com>
2023-10-08 16:50:54 +0300

    Добавил метод 'IndexOf'

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -29,8 +29,37 @@ void PrintArray(int[] col)
     }
 }
 
-int[] array = new int[10]; // Смысл конструкции "new int[10]" - создать новый массив из 10 элементов
+// * Метод поиска позиции заданного элемента в массиве
+
+// Код отработал с ошибками, результат не выдан
+
+int IndexOf(int collection, int find)
+{
+    int count = collection.Length; // "int" не содержит определения "Length", 
+    // и не удалось найти доступный метод расширения "Length", 
+    // принимающий тип "int" в качестве первого аргумента 
+    // (возможно, пропущена директива using или ссылка на сборку)
+    int index = 0;
+    int position = -1; // "-1" выводится на экран, в случае отсутствия элемента в массиве
+    
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // Завершить, как только будет найдена позиция первого заданного элемента
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // "new int[10]" означает: создать новый массив из 10 элементов
 
 FillArray(array); // Методу "FillArray" передаём массив "array"
 PrintArray(array); // "PrintArray" распечатает переданный массив
+Console.WriteLine(); // Пустая строка для разделения массива и индекса искомого элемента
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
 
