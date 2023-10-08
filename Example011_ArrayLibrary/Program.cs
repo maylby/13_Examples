0d94842e7ffefbb8fcccd3482dbf950cf26b396c
Vit <mayllby@gmail.com>
2023-10-08 14:47:52 +0300

    Добавил агоритм создания массива

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Лекция 2. Знакомство с функциями и массивами
+
+// 0:33:00
+
+int[] array = new int[10]; // Смысл конструкции "new int[10]": создать новый массив из 10 элементов
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // В агрумент "collection" на позицию "index" положить новое случайное целое число от 1 до 10
+        index++;
+    }
+}
