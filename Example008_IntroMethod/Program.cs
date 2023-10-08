6376025f2b51ebe7af45c9338dd2c405ec58a450
Vit <mayllby@gmail.com>
2023-10-08 10:09:42 +0300

    Добавил задачу 1

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,2 +1,29 @@
 ﻿// 13_(Л-02)
 // Лекция 2. Знакомство с функциями и массивами
+
+// 0:12:00
+// Задача 1
+
+int a1 = 13;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 313;
+
+int max = a1;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3;
+
+Console.WriteLine(max);
