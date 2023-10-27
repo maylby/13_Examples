001379d2937352fb1871ae25f64e07076995c3f1
Vit <mayllby@gmail.com>
2023-10-27 21:06:43 +0300

    Закрыл сессию

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -4,62 +4,62 @@
 
 // * Код создания массива
 
-void FillArray(int[] collection) // "void" - метод, который ничего не возвращает
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        // В агрумент "collection" на позицию "index" положить новое случайное целое число от 1 до 10
-        index++; // В методе "void" не применим оператор "return"
-    }
-}
+// void FillArray(int[] collection) // "void" - метод, который ничего не возвращает
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         // В агрумент "collection" на позицию "index" положить новое случайное целое число от 1 до 10
+//         index++; // В методе "void" не применим оператор "return"
+//     }
+// }
 
-// * Код вывода массива в печать (на экран)
+// // * Код вывода массива в печать (на экран)
 
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write(col[position] + " ");
-        position++;
-    }
-}
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(col[position] + " ");
+//         position++;
+//     }
+// }
 
-// * Метод поиска позиции заданного элемента в массиве
+// // * Метод поиска позиции заданного элемента в массиве
 
-// Код отработал с ошибками, результат не выдан
+// // Код отработал с ошибками, результат не выдан
 
-int IndexOf(int collection, int find)
-{
-    int count = collection.Length; // "int" не содержит определения "Length", 
-    // и не удалось найти доступный метод расширения "Length", 
-    // принимающий тип "int" в качестве первого аргумента 
-    // (возможно, пропущена директива using или ссылка на сборку)
-    int index = 0;
-    int position = -1; // "-1" выводится на экран, в случае отсутствия элемента в массиве
+// int IndexOf(int collection, int find)
+// {
+//     int count = collection.Length; // "int" не содержит определения "Length", 
+//     // и не удалось найти доступный метод расширения "Length", 
+//     // принимающий тип "int" в качестве первого аргумента 
+//     // (возможно, пропущена директива using или ссылка на сборку)
+//     int index = 0;
+//     int position = -1; // "-1" выводится на экран, в случае отсутствия элемента в массиве
     
-    while (index < count)
-    {
-        if (collection[index] == find)
-        {
-            position = index;
-            break; // Завершить, как только будет найдена позиция первого заданного элемента
-        }
-        index++;
-    }
-    return position;
-}
+//     while (index < count)
+//     {
+//         if (collection[index] == find)
+//         {
+//             position = index;
+//             break; // Завершить, как только будет найдена позиция первого заданного элемента
+//         }
+//         index++;
+//     }
+//     return position;
+// }
 
-int[] array = new int[10]; // "new int[10]" означает: создать новый массив из 10 элементов
+// int[] array = new int[10]; // "new int[10]" означает: создать новый массив из 10 элементов
 
-FillArray(array); // Методу "FillArray" передаём массив "array"
-PrintArray(array); // "PrintArray" распечатает переданный массив
-Console.WriteLine(); // Пустая строка для разделения массива и индекса искомого элемента
+// FillArray(array); // Методу "FillArray" передаём массив "array"
+// PrintArray(array); // "PrintArray" распечатает переданный массив
+// Console.WriteLine(); // Пустая строка для разделения массива и индекса искомого элемента
 
-int pos = IndexOf(array, 4);
-Console.WriteLine(pos);
+// int pos = IndexOf(array, 4);
+// Console.WriteLine(pos);
 
