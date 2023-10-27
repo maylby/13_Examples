22f7a07108b2b88174aa8ebf6f755ddef1d1240b
Vit <mayllby@gmail.com>
2023-10-27 20:54:43 +0300

    Закрыл сессию

--- Example005_Condition-If-Else/Program.cs
+++ Example005_Condition-If-Else/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Приветствие пользователя
 
-Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
+// Console.Write("Введите ваше имя: ");
+// string username = Console.ReadLine();
 
-if(username.ToLower() == "маша") // "ToLower" - перевод символов в нижний регистр
-{
-    Console.WriteLine("Ура, это же МАША!");
-}
-else
-{
-    Console.WriteLine("Привет, ");
-    Console.WriteLine(username);
-}
+// if(username.ToLower() == "маша") // "ToLower" - перевод символов в нижний регистр
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.WriteLine("Привет, ");
+//     Console.WriteLine(username);
+// }
