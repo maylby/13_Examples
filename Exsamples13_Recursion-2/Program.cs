08853d1a3bc8a3f03b56982cbe3e8df3bf3e726f
Vit <mayllby@gmail.com>
2023-10-27 23:17:05 +0300

    Добавил общее решение для перебора слов

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -124,19 +124,38 @@
 // 00:12:00
 // Перебор слов
 
-char[] s = { 'a', 'b', 'c', 'd'};
-int count = s.Length;
-int n = 1;
-for (int i = 0; i < count; i++)
-{
-    for (int j = 0; j < count; j++)
-    {
-        for (int k = 0; k < count; k++)
-        {
-            for (int l = 0; l < count; l++)
-            {
-                System.Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]{s[l]}}");
-            }
-        }
-    }
-}
+// char[] s = { 'a', 'b', 'c', 'd'};
+// int count = s.Length;
+// int n = 1;
+// for (int i = 0; i < count; i++)
+// {
+//     for (int j = 0; j < count; j++)
+//     {
+//         for (int k = 0; k < count; k++)
+//         {
+//             for (int l = 0; l < count; l++)
+//             {
+//                 System.Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}");
+//             }
+//         }
+//     }
+// }
+
+// // 00:16:00
+
+// // Общее решение задачи перебора слов
+
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if (length == word.Length)
+//     {
+//         System.Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for (int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length + 1);
+//     }
+// }
+
+// FindWords(abcd, new char[5]);
