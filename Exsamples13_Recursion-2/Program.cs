7232b44e163c20ab7dac50901e54245d51f99b50
Vit <mayllby@gmail.com>
2023-10-27 22:49:52 +0300

    Добавил сокращённое возведение в степень

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -108,3 +108,16 @@
 //     if (n == 0) return 1;
 //     else return PowerRec(a, n - 1) * a;
 // }
+
+// int PowerRecMath(int a, int n) // Сокращённое возведение в степень (см. скриншот "13_(Л-07)_img")
+// {
+//     if (n == 0) return 1;
+//     else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+//     else return PowerRecMath(a, n - 1) * a;
+// }
+
+// System.Console.WriteLine(PowerFor(2, 10)); // 1024
+// System.Console.WriteLine(PowerRec(2, 10)); // 1024
+// System.Console.WriteLine(PowerRecMath(2, 10));
+
+
