1c7e3045eb7c92c614622bcf8848fef7d8c2ba78
Vit <mayllby@gmail.com>
2023-10-27 21:29:23 +0300

    Добавил код 'snring'

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 13_(Л-07) Рекурсия продолжение
+
+// 00:04:30
+
+// Собрать строку с числами от a до b, a <= b
+
+// Код:
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = 0; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersFor(1, 10));
+System.Console.WriteLine(NumbersRec(1, 10));
