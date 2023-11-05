9921780f485223937f720ae68c1a64d2cf9d1451
Vit <mayllby@gmail.com>
2023-11-05 21:23:44 +0300

    Добавил 'Обход дерева'

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -231,10 +231,31 @@
 // 00:27:00
 // "Ханойские башни"
 
-void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
-{
-    if (count > 1) Towers(with, some, on, count - 1);
-    System.Console.WriteLine($"{with} >> {on}");
-    if (count > 1) Towers(some, on, with, count - 1);
-}
-Towers();
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     System.Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, with, count - 1);
+// }
+// Towers();
+
+
+// 00:29:00
+// Обход структур
+// https://en.wikipedia.org/wiki/Tree_traversal - "Обход дерева" (статья)
+
+// string emp = String.Empty;
+// string[] tree = {emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+// //                0    1    2     3    4    5    6    7    8    9    10   11
+// void InOrderTraversal(int pos = 1)
+// {
+//     if (pos < tree.Length)
+//     {
+//         int left = 2 * pos;
+//         int rigth = 2 * pos + 1;
+//          if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+//         System.Console.Write(tree[pos]);
+//         if (rigth < tree.Length && !String.IsNullOrEmpty(tree[rigth])) InOrderTraversal(rigth);
+//     }
+// }
+// InOrderTraversal();
