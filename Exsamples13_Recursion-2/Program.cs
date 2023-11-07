c4f182355ee07f3166774e6b01a4d5e003cbeef5
Vit <mayllby@gmail.com>
2023-11-07 12:36:20 +0300

    Добавил рекурсию и итерацию чисел Фибоначчи

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -262,3 +262,49 @@
 //     }
 // }
 // InOrderTraversal();
+
+
+// 00:38:26
+
+// Числа Фибоначчи
+// Нахождение последовательности двумя методами, рекурсивным и итерационным
+
+// decimal fRec = 0;
+// decimal fIte = 0;
+
+// decimal FibonacciRecursion(int n)
+// {
+//     fRec++;
+//     return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+// }
+
+// decimal FibonacciIteration(int n)
+// {
+//     fIte++;
+//     decimal result = 1;
+//     decimal f0 = 1;
+//     decimal f1 = 1;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         result = f0 + f1;
+
+//         f0 = f1;
+//         f1 = result;
+//         fIte++;
+//     }
+//     return result;
+// }
+
+// Console.ReadLine();
+// for (int n = 1; n <= 40; n++)
+// {
+//     Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte}");
+//     fIte = 0;
+// }
+// System.Console.WriteLine();
+// Console.ReadLine();
+// for (int n = 1; n <= 40; n++)
+// {
+//     Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec}");
+//     fRec = 0;
+// }
