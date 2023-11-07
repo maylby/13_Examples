2df1964e21bd2fa10da91bcf45382d988e81921c
Vit <mayllby@gmail.com>
2023-11-07 12:57:39 +0300

    Добавил счётчик времени в Фибоначчи

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -269,42 +269,47 @@
 // Числа Фибоначчи
 // Нахождение последовательности двумя методами, рекурсивным и итерационным
 
-// decimal fRec = 0;
-// decimal fIte = 0;
+decimal fRec = 0;
+decimal fIte = 0;
 
-// decimal FibonacciRecursion(int n)
-// {
-//     fRec++;
-//     return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
-// }
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
 
-// decimal FibonacciIteration(int n)
-// {
-//     fIte++;
-//     decimal result = 1;
-//     decimal f0 = 1;
-//     decimal f1 = 1;
-//     for (int i = 2; i <= n; i++)
-//     {
-//         result = f0 + f1;
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
 
-//         f0 = f1;
-//         f1 = result;
-//         fIte++;
-//     }
-//     return result;
-// }
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+DateTime dt = DateTime.Now;
 
-// Console.ReadLine();
-// for (int n = 1; n <= 40; n++)
-// {
-//     Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte}");
-//     fIte = 0;
-// }
-// System.Console.WriteLine();
-// Console.ReadLine();
-// for (int n = 1; n <= 40; n++)
-// {
-//     Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec}");
-//     fRec = 0;
-// }
+Console.ReadLine();
+for (int n = 1; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte}");
+    fIte = 0;
+}
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+System.Console.WriteLine();
+Console.ReadLine();
+dt = DateTime.Now;
+for (int n = 1; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec}");
+    fRec = 0;
+}
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
