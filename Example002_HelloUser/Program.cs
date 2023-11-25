28cee81e7a9335337d1c659779ffde9a55d147f2
Vit <mayllby@gmail.com>
2023-11-25 15:23:41 +0300

    Добавил комментарий

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Вывод текста столбиком ("WriteLine"):
-//Console.WriteLine("Введите ваше имя");
-// string username = Console.ReadLine();
-// Console.WriteLine("Привет, ");
-// Console.WriteLine(username);
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, "); // Не выводит введенное имя, хотя раньше выводил
+Console.WriteLine(username);
 
 // Вывод текста одной строкой ("Write")
-// Console.Write("Введите ваше имя: ");
-// string username = Console.ReadLine();
-// Console.Write("Привет, ");
-// Console.Write(username);
+Console.Write("Введите ваше имя: ");
+string username2 = Console.ReadLine();
+Console.Write("Привет, "); // Не выводит введенное имя
+Console.Write(username2);
