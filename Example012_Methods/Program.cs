96995b180710c4df96264cd13a2b29abc8189fbb
Vit <mayllby@gmail.com>
2023-11-26 21:19:29 +0300

    добавил 'Метод 2'

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -2,13 +2,21 @@
 // Урок 3. Функции: продолжение
 // https://gb.ru/lessons/362972
 
-// Вид 1 
 
-void Method()
+// Вид 1 Не принимающий и не возвращающий метод
+void Method1()
 {
-    System.Console.WriteLine("Автор ");
+    System.Console.WriteLine("Автор ...");
 }
-Method(); // вызов метода
+Method1(); // вызов метода
 
 
-// Вид 2
+// Вид 2 Принимающий, но не возвращающий метод
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+
+
