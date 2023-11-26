c8fb5dfd7aec54d056450d939057db5c7e6d5899
Vit <mayllby@gmail.com>
2023-11-26 16:58:36 +0300

    закрыл сессию

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -18,7 +18,8 @@ int Max(int arg1,int arg2, int arg3)
     if(arg3 > result) result = arg3;
     return result;
 } //            0   1   2    3   4    5     6   7     8 // индексы массива
-int[] array = {11, 21, 31, 411, 15, 612, 1117, 18, 1911};
+int[] array = {11, 21, 31, 411, 15, 612, 9117, 18, 1911};
+
 
 // можно дать любое имя переменной, присваивающей значение "Max(...)",
 // например, изменить "max" на "result"
@@ -28,5 +29,6 @@ Max(array[0], array[1], array[2]),
 Max(array[3], array[4], array[5]),
 Max(array[6], array[7], array[8])
 );
+
 // Console.WriteLine(max);
 Console.WriteLine(result);
