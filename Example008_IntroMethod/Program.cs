3a2a79067b62a9c7eea77339f120c9f489497833
Vit <mayllby@gmail.com>
2023-11-26 16:29:45 +0300

    comment

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -20,20 +20,25 @@ int Max(int arg1,int arg2, int arg3)
 
 int a1 = 13;
 int b1 = 21;
-int c1 = 39;
-int a2 = 1211111;
+int c1 = 391111;
+int a2 = 12;
 int b2 = 23;
 int c2 = 32123;
 int a3 = 13;
 int b3 = 2113;
-int c3 = 313;
+int c3 = 11111313;
 
 // int max1 = Max(a1, b1, c1);
 // int max2 = Max(a2, b2, c2);
 // int max3 = Max(a3, b3, c3);
 // int max = Max(max1, max2, max3);
 
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+int max = Max(  
+    Max(a1, b1, c1),    
+    Max(a2, b2, c2),    
+    Max(a3, b3, c3));
+    // "Tab+Enter" - перенос части строки на новую строку
 Console.WriteLine(max);
 
 // * Решение стихийным методом
@@ -50,4 +55,4 @@ Console.WriteLine(max);
 // if(b3 > max) max = b3;
 // if(c3 > max) max = c3;
 
-// Console.WriteLine(max1); // Строка используется для обоих приведённых выше методов
+// Console.WriteLine(max); // Строка используется для обоих приведённых выше методов
