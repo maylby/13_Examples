d363b3a23bd1ebc8187ca84c61b6a1087f8cf963
Vit <mayllby@gmail.com>
2023-11-27 01:50:29 +0300

    открыл коммиты

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,165 +1,165 @@
-﻿// // Знакомство с языками программирования (лекции)
-// // Урок 3. Функции: продолжение
-// // https://gb.ru/lessons/362972
+﻿// Знакомство с языками программирования (лекции)
+// Урок 3. Функции: продолжение
+// https://gb.ru/lessons/362972
 
-// // Методы условно делятся на возвращающие и невозвращающие
+// Методы условно делятся на возвращающие и невозвращающие
 
-// // 1) Невозвращающие методы - это те, которые 
-// // * Ничего не принимают и ничего не возвращают
-// // * Что-то принимают, но ничего не возвращают
-// // 2) Возвращающие методы - это те которые
-// // * (?) Ничего не принимают, но что-то возваращают 
-// // * Что-то принимают и что-то возвращают
-
-
-// // Вид 1 Не принимающий и не возвращающий метод
-// void Method1()
-// {
-//     System.Console.WriteLine("Автор ...");
-// }
-// Method1(); // вызов метода
+// 1) Невозвращающие методы - это те, которые 
+// * Ничего не принимают и ничего не возвращают
+// * Что-то принимают, но ничего не возвращают
+// 2) Возвращающие методы - это те которые
+// * (?) Ничего не принимают, но что-то возваращают 
+// * Что-то принимают и что-то возвращают
 
 
-// // Вид 2 Принимающий, но не возвращающий метод
-// void Method2(string msg)
-// {
-//     System.Console.WriteLine(msg);
-// }
-// Method2("Текст сообщения"); // вызов метода
-// Console.WriteLine(); // Пустая строка
+// Вид 1 Не принимающий и не возвращающий метод
+void Method1()
+{
+    System.Console.WriteLine("Автор ...");
+}
+Method1(); // вызов метода
 
 
-// // Вид 2.1 Именованный метод (принрмает, но не возвращает)
-// void Method21(string msg, int count)
-// {
-//     int i = 0;
-//     while (i < count)
-//     {
-//         System.Console.WriteLine(msg);
-//         i++;
-//     }
-// }
-// Method21("Текст", 4); // Неименованные аргументы пишутся в строгом порядке
-// Method21(count: 4, msg: "новый текст"); // Именованные - в произвольном
-// Console.WriteLine(); // Пустая строка - разрыв
+// Вид 2 Принимающий, но не возвращающий метод
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2("Текст сообщения"); // вызов метода
+Console.WriteLine(); // Пустая строка
 
 
-// // Вид 3 Не принимающий, но возвращающий метод
-// int Method3()
-// {
-//     return DateTime.Now.Year;
-// }
-// int year = Method3();
-// System.Console.WriteLine(year);
-// Console.WriteLine(); // Пустая строка
+// Вид 2.1 Именованный метод (принрмает, но не возвращает)
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст", 4); // Неименованные аргументы пишутся в строгом порядке
+Method21(count: 4, msg: "новый текст"); // Именованные - в произвольном
+Console.WriteLine(); // Пустая строка - разрыв
 
 
-// // Вид Принимающий и возвращающий метод
-// string Method4(int count, string text)
-// {
-//     int i = 0;
-//     string result = String.Empty; // "Empty" - пустая строка ("String") для записи "result"
-
-//     while (i < count)
-//     {
-//         result += text;
-//         i++;
-//     }
-//     return result;
-// }
-
-// string res = Method4(5, "z");
-// System.Console.WriteLine(res);
+// Вид 3 Не принимающий, но возвращающий метод
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+System.Console.WriteLine(year);
+Console.WriteLine(); // Пустая строка
 
 
-// // Цикл "for"
+// Вид Принимающий и возвращающий метод
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // "Empty" - пустая строка ("String") для записи "result"
 
-// string Method4For(int count, string text)
-// {
-//     string result = String.Empty;
-//     for (int i = 0; i < count; i++)
-//     {
-//         result += text;
-//     }
-//     return result;
-// }
+    while (i < count)
+    {
+        result += text;
+        i++;
+    }
+    return result;
+}
 
-// string res2 = Method4For(5, "z");
-// System.Console.WriteLine(res2);
-// Console.WriteLine(); // Пустая строка
-
-
-// // 00:18:40
-// // Цикл в цикле
-
-// // Таблица умножения
-
-// for (int i = 2; i <= 10; i++)
-// {
-//     for (int j = 2; j <= 10; j++)
-//     {
-//         System.Console.WriteLine($"{i} x {j} = {i*j}");
-//     }
-//     Console.WriteLine(); // Пустая строка
-// }
+string res = Method4(5, "z");
+System.Console.WriteLine(res);
 
 
-// // 0:20:30
+// Цикл "for"
 
-// // Решение задач с помощью цикла "for"
+string Method4For(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
 
-// // * Тренировочная задача
-// // // Задание на форматирование текста
+string res2 = Method4For(5, "z");
+System.Console.WriteLine(res2);
+Console.WriteLine(); // Пустая строка
 
-// // Дан текст. 
-// // В тексте все пробелы нужно заменить чёрточками.
-// // Маленькие буквы "к" заменить большими "К".
-// // Большие "С" заменить маленькими "с".
 
-// // Ясна ли задача?
+// 00:18:40
+// Цикл в цикле
 
-// // Что значит дан текст?
-// // Ввёл пользователь, считали из базы данных, скопировали с сервиса???
-// // *В нашем случае, текст будет храниться, как отдельная строка.
-// // Что значит чёрточками?
-// // Тире, дефис, минус, нижнее подчёркивание, слеш (правый, левый, вертикальный)??? 
-// // Какого алфавита буквы "к", "К", "с", "С" ???
+// Таблица умножения
 
-// // Ясна ли задача?
-// Console.WriteLine();
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine(); // Пустая строка
+}
 
-// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-//                 + "ежели бы вас послали вместо нашего милого Винцегероде, "
-//                 + "вы бы взяли приступом согласие русского короля. "
-//                 + "Вы так красноречивы. Вы дадите мне чаю?";
 
-// // string s = "qwerty"  (Подряд набранные знаки клавиатуры) 
-// //             012      (индексы знаков строки, начиная с нуля)
-// // s[3] // r            ("r" - 4-ый символ имеет индекс "3")
+// 0:20:30
 
-// string Replase(string text, char oldValue, char newValue)
-// {
-// 	string result = String.Empty;
-// 	int length = text.Length;
-// 	for (int i = 0; i < length; i++)
-// 	{
-// 		if(text[i] == oldValue) result = result + $"{newValue}";
-// 		else result = result + $"{text[i]}";
-// 	}
-// 	return result;
-// }
+// Решение задач с помощью цикла "for"
 
-// string newText = Replase(text, ' ', '|');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+// * Тренировочная задача
+// // Задание на форматирование текста
 
-// newText = Replase(newText, 'к', 'К');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+// Дан текст. 
+// В тексте все пробелы нужно заменить чёрточками.
+// Маленькие буквы "к" заменить большими "К".
+// Большие "С" заменить маленькими "с".
 
-// newText = Replase(newText, 'с', '*');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+// Ясна ли задача?
+
+// Что значит дан текст?
+// Ввёл пользователь, считали из базы данных, скопировали с сервиса???
+// *В нашем случае, текст будет храниться, как отдельная строка.
+// Что значит чёрточками?
+// Тире, дефис, минус, нижнее подчёркивание, слеш (правый, левый, вертикальный)??? 
+// Какого алфавита буквы "к", "К", "с", "С" ???
+
+// Ясна ли задача?
+Console.WriteLine();
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+                + "ежели бы вас послали вместо нашего милого Винцегероде, "
+                + "вы бы взяли приступом согласие русского короля. "
+                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"  (Подряд набранные знаки клавиатуры) 
+//             012      (индексы знаков строки, начиная с нуля)
+// s[3] // r            ("r" - 4-ый символ имеет индекс "3")
+
+string Replase(string text, char oldValue, char newValue)
+{
+	string result = String.Empty;
+	int length = text.Length;
+	for (int i = 0; i < length; i++)
+	{
+		if(text[i] == oldValue) result = result + $"{newValue}";
+		else result = result + $"{text[i]}";
+	}
+	return result;
+}
+
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replase(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replase(newText, 'с', '*');
+Console.WriteLine(newText);
+Console.WriteLine();
 
 
 // 28:30
