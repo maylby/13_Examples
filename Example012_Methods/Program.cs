919f151ed8b751c7ff4076eb0c6978fabe4dacf6
Vit <mayllby@gmail.com>
2023-11-26 23:11:23 +0300

    добавил пояснение

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,107 +1,107 @@
-﻿// Знакомство с языками программирования (лекции)
-// Урок 3. Функции: продолжение
-// https://gb.ru/lessons/362972
+﻿// // Знакомство с языками программирования (лекции)
+// // Урок 3. Функции: продолжение
+// // https://gb.ru/lessons/362972
 
-// Методы условно делятся на возвращающие и невозвращающие
+// // Методы условно делятся на возвращающие и невозвращающие
 
-// 1) Невозвращающие методы - это те, которые 
-// * Ничего не принимают и ничего не возвращают
-// * Что-то принимают, но ничего не возвращают
-// 2) Возвращающие методы - это те которые
-// * (?) Ничего не принимают, но что-то возваращают 
-// * Что-то принимают и что-то возвращают
-
-
-// Вид 1 Не принимающий и не возвращающий метод
-void Method1()
-{
-    System.Console.WriteLine("Автор ...");
-}
-Method1(); // вызов метода
+// // 1) Невозвращающие методы - это те, которые 
+// // * Ничего не принимают и ничего не возвращают
+// // * Что-то принимают, но ничего не возвращают
+// // 2) Возвращающие методы - это те которые
+// // * (?) Ничего не принимают, но что-то возваращают 
+// // * Что-то принимают и что-то возвращают
 
 
-// Вид 2 Принимающий, но не возвращающий метод
-void Method2(string msg)
-{
-    System.Console.WriteLine(msg);
-}
-Method2("Текст сообщения"); // вызов метода
-Console.WriteLine(); // Пустая строка
+// // Вид 1 Не принимающий и не возвращающий метод
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор ...");
+// }
+// Method1(); // вызов метода
 
 
-// Вид 2.1 Именованный метод (принрмает, но не возвращает)
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        System.Console.WriteLine(msg);
-        i++;
-    }
-}
-Method21("Текст", 4); // Неименованные аргументы пишутся в строгом порядке
-Method21(count: 4, msg: "новый текст"); // Именованные - в произвольном
-Console.WriteLine(); // Пустая строка - разрыв
+// // Вид 2 Принимающий, но не возвращающий метод
+// void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения"); // вызов метода
+// Console.WriteLine(); // Пустая строка
 
 
-// Вид 3 Не принимающий, но возвращающий метод
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-int year = Method3();
-System.Console.WriteLine(year);
-Console.WriteLine(); // Пустая строка
+// // Вид 2.1 Именованный метод (принрмает, но не возвращает)
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21("Текст", 4); // Неименованные аргументы пишутся в строгом порядке
+// Method21(count: 4, msg: "новый текст"); // Именованные - в произвольном
+// Console.WriteLine(); // Пустая строка - разрыв
 
 
-// Вид Принимающий и возвращающий метод
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty; // "Empty" - пустая строка ("String") для записи "result"
-
-    while (i < count)
-    {
-        result += text;
-        i++;
-    }
-    return result;
-}
-
-string res = Method4(5, "z");
-System.Console.WriteLine(res);
+// // Вид 3 Не принимающий, но возвращающий метод
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// System.Console.WriteLine(year);
+// Console.WriteLine(); // Пустая строка
 
 
-// Цикл "for"
+// // Вид Принимающий и возвращающий метод
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; // "Empty" - пустая строка ("String") для записи "result"
 
-string Method4For(int count, string text)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result += text;
-    }
-    return result;
-}
+//     while (i < count)
+//     {
+//         result += text;
+//         i++;
+//     }
+//     return result;
+// }
 
-string res2 = Method4For(5, "z");
-System.Console.WriteLine(res2);
-Console.WriteLine(); // Пустая строка
+// string res = Method4(5, "z");
+// System.Console.WriteLine(res);
 
 
-// 00:18:40
-// Цикл в цикле
+// // Цикл "for"
 
-// Таблица умножения
+// string Method4For(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result += text;
+//     }
+//     return result;
+// }
 
-for (int i = 2; i <= 10; i++)
-{
-    for (int j = 2; j <= 10; j++)
-    {
-        System.Console.WriteLine($"{i} x {j} = {i*j}");
-    }
-    Console.WriteLine(); // Пустая строка
-}
+// string res2 = Method4For(5, "z");
+// System.Console.WriteLine(res2);
+// Console.WriteLine(); // Пустая строка
+
+
+// // 00:18:40
+// // Цикл в цикле
+
+// // Таблица умножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine(); // Пустая строка
+// }
 
 
 // 0:20:30
@@ -129,8 +129,14 @@ for (int i = 2; i <= 10; i++)
 
 string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
 + "ежели бы вас послали вместо нашего милого Винцегероде, "
-+ "вы бы взяли приступом согласие русского короляб. "
++ "вы бы взяли приступом согласие русского короля. "
 + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// 22:00
+ 
+// string s = "qwerty"  (Подряд набранные знаки клавиатуры) 
+//             012      (индексы знаков строки, начиная с нуля)
+// s[3] // r            ("r" - 4-ый символ имеет индекс "3")
 
 string Replase(string text, char oldValue, char newValue)
 {
@@ -147,9 +153,11 @@ string Replase(string text, char oldValue, char newValue)
 string newText = Replase(text, ' ', '|');
 Console.WriteLine(newText);
 Console.WriteLine();
+
 newText = Replase(text, 'к', 'К');
 Console.WriteLine(newText);
 Console.WriteLine();
+
 newText = Replase(text, 'С', 'с');
 Console.WriteLine(newText);
 Console.WriteLine();
