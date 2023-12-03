a76484ab223d2bca0d65befd77217c1fdcd24b03
Vit <mayllby@gmail.com>
2023-12-03 18:33:15 +0300

    Добавил числа Фибоначчи

--- Exsamples13_Recursion-1/Program.cs
+++ Exsamples13_Recursion-1/Program.cs
@@ -110,24 +110,42 @@
 // PrintImage(pic);
 
 
-// 00:25:00
-// Рекурсия
+// // 00:25:00
+// // Рекурсия
 
-// Факториал числа
-// 5! = 5*4*3*2*1
-    // 5*4!
-        // 4*3!
-            // 3*2!
-                // 2*1!
+// // Факториал числа
+// // 5! = 5*4*3*2*1
+//     // 5*4!
+//         // 4*3!
+//             // 3*2!
+//                 // 2*1!
 
-double Factorial(int n) // заменил тип данных "int" на "double" для больших чисел
+// double Factorial(int n) // заменил тип данных "int" на "double" для больших чисел
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if(n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// 00:31:30
+// Числа Фибоначчи (вычисление через рекурсию)
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-1)
+
+int Fibonacci(int n)
 {
-    // 1! = 1
-    // 0! = 1
-    if(n == 1) return 1;
-    else return n * Factorial(n - 1);
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
-for (int i = 1; i < 40; i++)
+for (int i = 1; i < 50; i++)
 {
-    Console.WriteLine($"{i}! = {Factorial(i)}");
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
