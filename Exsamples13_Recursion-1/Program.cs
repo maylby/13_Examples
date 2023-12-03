308b31d073e275d443346fdfc9643e947cec3cc6
Vit <mayllby@gmail.com>
2023-12-03 18:12:42 +0300

    Изменил код

--- Exsamples13_Recursion-1/Program.cs
+++ Exsamples13_Recursion-1/Program.cs
@@ -127,5 +127,7 @@ double Factorial(int n) // заменил тип данных "int" на "double
     if(n == 1) return 1;
     else return n * Factorial(n - 1);
 }
-
-System.Console.WriteLine(Factorial(5));
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
