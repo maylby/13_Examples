b09187ed68f0da8cb8a1d04546f3a35673fc0e14
Vit <mayllby@gmail.com>
2023-12-03 11:04:45 +0300

    Добавил метод печати

--- Exsamples13_Recursion-1/Program.cs
+++ Exsamples13_Recursion-1/Program.cs
@@ -18,8 +18,6 @@
 // }
 
 
-int[,] matrix = new int[3, 4];
-
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
@@ -30,6 +28,8 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 }
 System.Console.WriteLine();
 
+
+// Метод печати (вывод на экран)
 
 void PrintArray(int[,] matr)
 {
@@ -43,4 +43,7 @@ void PrintArray(int[,] matr)
     }
 }
 
+
+
+int[,] matrix = new int[3, 4];
 PrintArray(matrix);
