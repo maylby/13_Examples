a81aa855ea6dabe372115b54dfba974e72d871af
Vit <mayllby@gmail.com>
2023-12-04 14:45:56 +0300

    Добавил пример кода

--- Example014_NiceCode/Program.cs
+++ Example014_NiceCode/Program.cs
@@ -36,3 +36,31 @@ DrawText(
 
 string label = ""; // не правильный (малопонятный) способ инициализации переменной
 string address = String.Empty; // инициализация в виде константы "Пустая строка" ("String.Empty")
+
+
+// 00:29:00
+// Копипаст кода
+
+// Пример 1
+double a = 1, b = -26, c = 120;
+var d = b * b - 4 * a * c;
+double x1 = (-b + Math.Sgrt(d)) / (2 * a);
+double x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+// Пример 2
+a = 2, b = 1, c = -3;
+d = b * b - 4 * a * c;
+x1 = (-b + Math.Sgrt(d)) / (2 * a);
+x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+// Пример 3
+a = 1, b = 1, c = -6;
+d = b * b - 4 * a * c;
+x1 = (-b + Math.Sgrt(d)) / (2 * a);
+x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+
+
