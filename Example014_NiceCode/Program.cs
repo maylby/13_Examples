b9cb70ec872154e1a4fb0939ae4e2c3e14e59b02
Vit <mayllby@gmail.com>
2023-12-04 18:56:02 +0300

    Завершил сессию

--- Example014_NiceCode/Program.cs
+++ Example014_NiceCode/Program.cs
@@ -1,80 +1,80 @@
-﻿// // Знакомство с языками программирования (лекции)
-// // Урок 6. Как не нужно писать код: принципы и антипаттерны
-// // https://gb.ru/lessons/362975
+﻿// Знакомство с языками программирования (лекции)
+// Урок 6. Как не нужно писать код: принципы и антипаттерны
+// https://gb.ru/lessons/362975
 
-// // 00:23:20
+// 00:23:20
 
-// void DrawText(string text, int left, int top)
-// {
-//     Console.SetCursorPosition(left, top);
-//     Console.WriteLine(text);
-// }
+void DrawText(string text, int left, int top)
+{
+    Console.SetCursorPosition(left, top);
+    Console.WriteLine(text);
+}
 
-// DrawText("Intensive C# Demo text", 629, 360); // Непонятные числа!
-
-
-// // 00:26:56
-// // Улучшение кода
-
-// string caption = "Intensive C# Demo text";
-// int screenWidthPosition = (Console.WindowWidth - caption.Length / 2);
-// // Смысл: Позиция по длине экрана равна разности длины экрана и длины строки делённой пополам 
-// int screenHeightPosition = Console.WindowHeigth / 2; // Аналогично с позицией по высоте
-
-// DrawText(caption, screenWidthPosition, screenHeightPosition); 
-// // Передаём полученные значения методу для последующей обработки
-// // Ниже, разделение кода на смысловые составляющие для удобства последующей работы
-// DrawText(
-//     text: caption, 
-//     left: screenWidthPosition, 
-//     top: screenHeightPosition
-//     ); // Логика кода стала более понятной
+DrawText("Intensive C# Demo text", 629, 360); // Непонятные числа!
 
 
-// // 00:29:00
-// // Инициализация строки (переменной)
+// 00:26:56
+// Улучшение кода
 
-// string label = ""; // не правильный (малопонятный) способ инициализации переменной
-// string address = String.Empty; // инициализация в виде константы "Пустая строка" ("String.Empty")
+string caption = "Intensive C# Demo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length / 2);
+// Смысл: Позиция по длине экрана равна разности длины экрана и длины строки делённой пополам 
+int screenHeightPosition = Console.WindowHeigth / 2; // Аналогично с позицией по высоте
 
-
-// // 00:29:00
-// // Копипаст кода
-
-// // Пример 1
-// double a = 1, b = -26, c = 120;
-// var d = b * b - 4 * a * c;
-// double x1 = (-b + Math.Sgrt(d)) / (2 * a);
-// double x2 = (-b + Math.Sgrt(d)) / (2 * a);
-// Console.WriteLine($"x1 = {x1} x2 = {x2}");
-
-// // Пример 2
-// a = 2, b = 1, c = -3;
-// d = b * b - 4 * a * c;
-// x1 = (-b + Math.Sgrt(d)) / (2 * a);
-// x2 = (-b + Math.Sgrt(d)) / (2 * a);
-// Console.WriteLine($"x1 = {x1} x2 = {x2}");
-
-// // Пример 3
-// a = 1, b = 1, c = -6;
-// d = b * b - 4 * a * c;
-// x1 = (-b + Math.Sgrt(d)) / (2 * a);
-// x2 = (-b + Math.Sgrt(d)) / (2 * a);
-// Console.WriteLine($"x1 = {x1} x2 = {x2}");
+DrawText(caption, screenWidthPosition, screenHeightPosition); 
+// Передаём полученные значения методу для последующей обработки
+// Ниже, разделение кода на смысловые составляющие для удобства последующей работы
+DrawText(
+    text: caption, 
+    left: screenWidthPosition, 
+    top: screenHeightPosition
+    ); // Логика кода стала более понятной
 
 
-// // 00:33:10
-// // Отдельный метод вместо копипаста кода
+// 00:29:00
+// Инициализация строки (переменной)
 
-// public Roots Solve(double a, double b, double c) 
-// // Метод принимает три коэффициента и возвращает корни
-// {
-//     var d = b * b - 4 * a * c;
-//     double x1 = (-b + Math.Sgrt(d)) / (2 * a);
-//     double x2 = (-b + Math.Sgrt(d)) / (2 * a);
-//     return new Roots {X1 = x1, X2 = x2};
-// }
-// // Написать метод и трижды его использовать, лучше, чем трижды копипастить код
+string label = ""; // не правильный (малопонятный) способ инициализации переменной
+string address = String.Empty; // инициализация в виде константы "Пустая строка" ("String.Empty")
+
+
+// 00:29:00
+// Копипаст кода
+
+// Пример 1
+double a = 1, b = -26, c = 120;
+var d = b * b - 4 * a * c;
+double x1 = (-b + Math.Sgrt(d)) / (2 * a);
+double x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+// Пример 2
+a = 2, b = 1, c = -3;
+d = b * b - 4 * a * c;
+x1 = (-b + Math.Sgrt(d)) / (2 * a);
+x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+// Пример 3
+a = 1, b = 1, c = -6;
+d = b * b - 4 * a * c;
+x1 = (-b + Math.Sgrt(d)) / (2 * a);
+x2 = (-b + Math.Sgrt(d)) / (2 * a);
+Console.WriteLine($"x1 = {x1} x2 = {x2}");
+
+
+// 00:33:10
+// Отдельный метод вместо копипаста кода
+
+public Roots Solve(double a, double b, double c) 
+// Метод принимает три коэффициента и возвращает корни
+{
+    var d = b * b - 4 * a * c;
+    double x1 = (-b + Math.Sgrt(d)) / (2 * a);
+    double x2 = (-b + Math.Sgrt(d)) / (2 * a);
+    return new Roots {X1 = x1, X2 = x2};
+}
+// Написать метод и трижды его использовать, лучше, чем трижды копипастить код
 
 // 00:41:00
 // Пример кода
