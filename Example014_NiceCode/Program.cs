ab7309a377d70fa82c831b8a8a9a697da65d39d2
Vit <mayllby@gmail.com>
2023-12-04 13:37:13 +0300

    Добавил код 'DrawText'

--- Example014_NiceCode/Program.cs
+++ Example014_NiceCode/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Знакомство с языками программирования (лекции)
 // Урок 6. Как не нужно писать код: принципы и антипаттерны
 // https://gb.ru/lessons/362975
+
+// 00:23:20
+
+void DrawText(string text, int left, int top)
+{
+    Console.SetCursorPosition(left, top);
+    Console.WriteLine(text);
+}
+
+DrawText("Intensive C# Demo text", 629, 360);
