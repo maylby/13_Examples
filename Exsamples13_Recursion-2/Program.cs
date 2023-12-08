d13fc7939549c003ee8d939dca3b4f6978fc1f52
Vit <mayllby@gmail.com>
2023-12-08 14:49:42 +0300

    Exsamples 13 Recursion-2

--- Exsamples13_Recursion-2/Program.cs
+++ Exsamples13_Recursion-2/Program.cs
@@ -394,7 +394,7 @@ void FillImage(int row, int col)
 {
     if (pic[row, col] == 0)
     {
-        pic[row, col] =1;
+        pic[row, col] = 1;
         FillImage(row - 1, col);
         FillImage(row, col - 1);
         FillImage(row + 1, col);
